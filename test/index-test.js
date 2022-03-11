622d93f692691ee7e432b57475a13be157baa6c1
mubarek78 <shifamubarek@yahoo.com>
2022-03-10 22:10:34 -0700

    Completed assignment

--- test/index-test.js
+++ test/index-test.js
@@ -6,6 +6,10 @@ describe('introduction(name)', function() {
   })
 })
 
+function introduction(name){
+ return ("Hi, my name is " + name + ".")
+}
+
 describe('introductionWithLanguage(name, language)', function() {
   it('takes in two arguments, a name and a language, and returns a phrase using those arguments', function() {
     expect(introductionWithLanguage("Aki", "Ember.js")).toEqual("Hi, my name is Aki and I am learning to program in Ember.js.");
@@ -13,11 +17,20 @@ describe('introductionWithLanguage(name, language)', function() {
   })
 })
 
+function introductionWithLanguage(name, language){
+  return ("Hi, my name is " + name + " and I am learning to program in " + language + ".")
+}
+
+
 describe('introductionWithLanguageOptional(name, language)', function() {
   it('takes in two arguments, a name and a language, and language defaults to JavaScript', function() {
     expect(introductionWithLanguageOptional("Gracie")).toEqual("Hi, my name is Gracie and I am learning to program in JavaScript.");
   })
 })
+
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+  return ("Hi, my name is " + name + " and I am learning to program in " + language + ".")
+}
 
 describe('introductionWithLanguageOptional(name, language)', function() {
   it('takes in two arguments, a name and a language, and the default value can be overridden with an argument', function() {
